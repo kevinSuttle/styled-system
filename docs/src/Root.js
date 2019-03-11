85d827cb01f3ca6387bc2ca9d0881dca31dbf545
Brent Jackson <jxnblk@gmail.com>
2019-03-11 11:27:53 -0400

    Attempt to get emotion working

--- docs/src/Root.js
+++ docs/src/Root.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { graphql, useStaticQuery } from 'gatsby'
 import { Helmet } from 'react-helmet'
-import Global from '@emotion/core'
+import { Global } from '@emotion/core'
 
 const style = (
   <Global
